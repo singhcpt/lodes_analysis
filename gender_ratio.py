c9f57d0d717573d2e6e1647c7e1044282ed7272a
singhcpt <singhcpt@gmail.com>
2019-02-17 17:52:25 -0500

    edge cases and comments added

--- gender_ratio.py
+++ gender_ratio.py
@@ -1,7 +1,14 @@
+"""
+The purpose of this file is to find the male to female ratioof all residents in Chester County 
+using 2015 LODES Census data. This script only uses one map reduce step to do this. The purpose 
+of this exercise is to familiarize ourselves with the LODES data format as well as check the 
+CSV file for any imperfections in the data. 
+"""
+
 from mrjob.job import MRJob
 from mrjob.step import MRStep
 import re
-#
+
 class gender_ratio(MRJob):
 	
 	def steps(self):
@@ -12,11 +19,20 @@ class gender_ratio(MRJob):
 
 	def gender_mapper(self, key, line):
 		cols = line.split(',')
+
+		# Checking if variables are missing
+		if (len(cols) <= 40):
+    			return
 		geocode = cols[0]
-		county = geocode[0:5]
-		print (geocode)
+		# Checking for invalid geocodes
+		if (len(geocode) < 15):
+    			return
+		county = geocode[0:5] # Stripping county codes only from the geocode variable format
 		numMale = cols[40]
 		numFemale = cols[41]
+		# Checking for invalid counts in columns
+		if (numMale == "") or (numFemale == ""):
+    			return
 		if county == "42029":
 			 yield county, (numMale, numFemale)
 
@@ -24,9 +40,9 @@ class gender_ratio(MRJob):
                 femaleCount = 0
                 maleCount = 0
                 for numMale, numFemale in values:
-                        femaleCount += numFemale
-                        maleCount += numMale
-                yield 'Sum', (maleCount, femaleCount)
+                        femaleCount += int(numFemale)
+                        maleCount += int(numMale)
+                yield 'Male to Female Ratio:', (maleCount, femaleCount)
 
 if __name__ == '__main__':
 	gender_ratio.run()
