2aa3d12ffe21a8bccbce9f9e641d47ba6f719262
singhcpt <singhcpt@gmail.com>
2019-02-21 09:49:58 -0500

    updated command for generality

--- rungen.sh
+++ rungen.sh
@@ -1,2 +1,2 @@
-hdfs dfs -rm -r GENDER_OUT
-python3 gender_ratio.py $MR_OPT --jobconf mapred.reduce.tasks=4 hdfs:///user/cms7202/pa_rac_S000_JT00_2015.csv --output-dir hdfs:///user/cms7202/GENDER_OUT_STATE_1 --no-output
+hdfs dfs -rm -r [INSERT_OUTPUT_DIR_NAME]
+python3 [INSERT_SCRIPT_NAME] $MR_OPT --jobconf mapred.reduce.tasks=4 hdfs:///user/[INSERT_USERNAME]/pa_rac_S000_JT00_2015.csv --output-dir hdfs:///user/[INSERT_USER_NAME]/[INSERT_OUTPUT_DIR_NAME] --no-output
